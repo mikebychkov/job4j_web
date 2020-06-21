da6536070228b1d9794320ba17cbe367fa04ad60
mikebychkov <mihailbichkov@yandex.ru>
2020-06-21 12:25:45 +0500

    1. Запуск нити. Thread#start() [#309114]

---
+++ Ch_01_Mulithreading/src/main/java/com/ch_01_threads/ex_01_thread_start/ConcurrentOutput.java
@@ -0,0 +1,16 @@
+package com.ch_01_threads.ex_01_thread_start;
+
+public class ConcurrentOutput {
+
+    public static void main(String[] args) {
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        another.start();
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        second.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
