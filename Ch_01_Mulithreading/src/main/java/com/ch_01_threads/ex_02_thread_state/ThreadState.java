2c267cc5a4a18de87cd81dd17679deb6931fbd56
mikebychkov <mihailbichkov@yandex.ru>
2020-06-21 12:46:20 +0500

    1.1. Состояние нити. [#309111]

---
+++ Ch_01_Mulithreading/src/main/java/com/ch_01_threads/ex_02_thread_state/ThreadState.java
@@ -0,0 +1,25 @@
+package com.ch_01_threads.ex_02_thread_state;
+
+public class ThreadState {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread first = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName());
+                }
+        );
+        first.start();
+
+        Thread second = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName());
+                }
+        );
+        second.start();
+
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+            Thread.sleep(100);
+        }
+        System.out.println("All jobs are complete!");
+    }
+}
