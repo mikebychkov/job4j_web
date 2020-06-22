8ffa6c2a9a2285a23b2598e3c2166b2f11e80208
mikebychkov <mihailbichkov@yandex.ru>
2020-06-23 00:59:43 +0500

    4. Скачивание файла с ограничением. [#309110] #2

--- Ch_01_Mulithreading/src/main/java/com/ch_01_threads/ex_06_wget/WGet.java
+++ Ch_01_Mulithreading/src/main/java/com/ch_01_threads/ex_06_wget/WGet.java
@@ -7,7 +7,7 @@ import java.net.URL;
 
 public class WGet {
 
-    public static void getFile(String file, int threshold) throws Exception {
+    private boolean download(String file, int threshold) {
         try (BufferedInputStream in = new BufferedInputStream(new URL(file).openStream());
              FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
             byte[] dataBuffer = new byte[threshold];
@@ -16,26 +16,37 @@ public class WGet {
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
                 Thread.sleep(1000);
             }
+            return true;
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public void start(String file, int threshhold) {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+
+        boolean rsl = download(file, threshhold);
+
+        progress.interrupt();
+
+        System.out.println();
+        if (rsl) {
+            System.out.println("Loading complete.");
+        } else {
+            System.out.println("Filed to get file.");
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         if (args.length < 2) {
             throw new IllegalArgumentException("Params must be: FileNameOrURL DownloadThresholdBytes");
         }
         String file = args[0];
         int threshhold = Integer.parseInt(args[1]);
-
-        Thread progress = new Thread(new ConsoleProgress());
-        progress.start();
-
-        getFile(file, threshhold);
-
-        progress.interrupt();
-
-        System.out.println();
-        System.out.println("Loading complete.");
+        new WGet().start(file, threshhold);
     }
 }
