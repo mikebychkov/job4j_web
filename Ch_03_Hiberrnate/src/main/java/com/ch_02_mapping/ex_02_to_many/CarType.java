5fcf6296f034f4b3876fe13f426ac1b0deb2377f
Admin <mihailbichkov@yandex.ru>
2020-08-30 12:05:17 +0500

    hibernate mapping

--- Ch_03_Hiberrnate/src/main/java/com/ch_02_mapping/ex_02_to_many/CarType.java
+++ Ch_03_Hiberrnate/src/main/java/com/ch_02_mapping/ex_02_to_many/CarType.java
@@ -2,12 +2,13 @@ package com.ch_02_mapping.ex_02_to_many;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class CarType {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     public CarType() {
--- Ch_03_Hiberrnate/src/main/java/com/ch_02_mapping/ex_03_to_many_bidirectional/Car.java
+++ Ch_03_Hiberrnate/src/main/java/com/ch_02_mapping/ex_03_to_many_bidirectional/Car.java
@@ -2,12 +2,13 @@ package com.ch_02_mapping.ex_03_to_many_bidirectional;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Car {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     public Car() {
